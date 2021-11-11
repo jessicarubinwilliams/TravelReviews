25115ad5b50fd404ea546d3c84c13740e6d02ddc
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2021-11-10 20:38:26 -0800

    add swagger

--- Travel/Startup.cs
+++ Travel/Startup.cs
@@ -1,10 +1,13 @@
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Travel.Models;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;  //for ApiVersion
 
 namespace Travel
 {
@@ -24,6 +27,27 @@ namespace Travel
             services.AddDbContext<TravelContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Travel",
+                    Description = "An ASP.NET Core Web API for managing Travel.Solution items",
+                    // TermsOfService = new System.Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Butte Group",
+                        // Url = new Uri("https://example.com/contact")
+                    },
+                    // License = new OpenApiLicense
+                    // {
+                    //     Name = "Example License",
+                    //     Url = new Uri("https://example.com/license")
+                    // }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +56,12 @@ namespace Travel
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); //sets swagger as the index route
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             // app.UseHttpsRedirection();
